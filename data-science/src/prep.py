11f40675dc5f5f146a3008d9ce4a7862b4c96ff7
arpansaha1980 <arpasaha@gmail.com>
2025-11-21 19:06:46 -0500

    Fix indentation for test_train_ratio argument

--- data-science/src/prep.py
+++ data-science/src/prep.py
@@ -13,7 +13,7 @@ def parse_args():
     parser.add_argument("--data", type=str, help="Path to raw data")  # Specify the type for raw data (str)
     parser.add_argument("--train_data", type=str, help="Path to train dataset")  # Specify the type for train data (str)
     parser.add_argument("--test_data", type=str, help="Path to test dataset")  # Specify the type for test data (str)
-   parser.add_argument("--test_train_ratio", type=float, default=0.2)  # Specify the type (float) and default value (0.2) for test-train ratio
+    parser.add_argument("--test_train_ratio", type=float, default=0.2)  # Specify the type (float) and default value (0.2) for test-train ratio
     args = parser.parse_args()
 
     return args
